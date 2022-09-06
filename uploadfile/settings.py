0c7c643bc2df7915d10886b36835046879ea204e
alfin <alfin@linkaja.id>
2022-09-06 18:26:43 +0700

    first update

--- uploadfile/settings.py
+++ uploadfile/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sbao@#l$ww3ufvah!lhsnk4x*a&el=^j$9lf7p08@x_^c1n51i'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
